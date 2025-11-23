2a8872c0affefed81065b43b3832ae502e26455f
TommyShinest <iamtommyshinest@gmail.com>
2025-11-23 15:28:23 -0400

    Update pipe room.js
    added water that fills the screen

--- pipe room.js
+++ pipe room.js
@@ -6,8 +6,24 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-
+  //background(220);
+  
+  // WATER
+  let waterWidth = width;
+  let waterHeight = 0;
+  let waterGrowthRate; // pixels per frame
+  let numOfPipes = 1; //placeholder
+  
+  waterGrowthRate = numOfPipes / 4;
+  
+  // starts at zero and increases
+  waterHeight = waterGrowthRate * frameCount
+  
+  fill("blue")
+  
+  // draws water starting at the bottom of the screen
+  rect(0, height - waterHeight, waterWidth, waterHeight);
+  
 }
 
-// this is a test of commits and branches and pull requests and grib
+// this is a test of commits and branches and pull requests and gribf
