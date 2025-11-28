2d649490db27ff4a62fb8038cd8684206e4e3aec
TommyShinest <iamtommyshinest@gmail.com>
2025-11-28 03:52:26 -0400

    Update pipe room.js
    //problems //water takes WAY too long to grow after clicking the first pipe, we haven't added clicking to any other pipes //the water should rise slower after clicking that pipe, not stop for a few seconds and continue
    
    pipes appear faster over time, (smaller gaps between appearances)
    
    clicking a pipe should decrement numOfPipes
    
    pipes are now organized into an array of objects, there were a lot of bugs there and i need wayy more practice using these gribs
    
    score
    
    added a background, but that means less water coming from pipes, very sad

--- pipe room.js
+++ pipe room.js
@@ -1,8 +1,73 @@
-// pipe room
 // this is for p5.js
 
+// Set difficulty here by editing the numbers (technically these are milliseconds)
+//EZ MODE: 20, 1400
+//I like 40 and 1200
+// HARD 50 1000
+// Impossible 100, 800
+let difficulty = 40; // bigger makes the end come faster
+let gameLengthener = 1200; // bigger makes the start eazier
+
+
+
+// Alter size of pipes
+let pipeGrower = 0;
+
+
+let pipeWidth = 20 + pipeGrower; // measured in pixels
+let pipeHeight = 50 + pipeGrower;
+
+// room drain
+let drainRate = 1 / 2;
+
+
+
+
+
+// PIPE OBJECT LIT MAKER
+
+let pipeList = [];
+
+function exclusionZone() {
+  let x = floor(random(0,2));// fun fact, the numbers are floats so they wont always be exactly one or two, so we make them exact numbers with floor()
+  if (x === 0|| x === -1) {
+    return random(0, 200-pipeWidth);// from left to firstPipe
+  } else if (x === 1 || x === 2) {
+    return random(220, width-pipeWidth);// from firstPipe to right
+  }
+}
+
+function newPipe() {
+  pipe = {
+    x: exclusionZone(),
+    boolIsClicked: function() {
+      return mouseX > this.x && mouseX < this.x + pipeWidth && mouseY < pipeHeight
+    },
+    disappear: function() {
+      fill('black');
+      rect(this.x, -1, pipeWidth, pipeHeight);
+    },
+    draw: function() {
+      strokeWeight(2);
+      fill('gray');
+      rect(this.x, -1, pipeWidth, pipeHeight);
+    },
+    dripY: 0
+  };
+
+  return pipe;
+}
 
 // GLOBAL VARS
+let noTimer = false;
+
+let timeGap;
+
+let pipesClicked = 0;
+
+let timeSurvived = 0;
+
+let firstPipeIsOnScreen = true;
 
 let gameRunning = false;
 
@@ -27,65 +92,65 @@ let numOfPipes = 1;
 let noPipes = false;
 
 
+
 function setup() {
   createCanvas(400, 400,);
-  strokeWeight(0);
+  noStroke();
   textSize(22);
 }
 
-let pipeX = 0;
+
 
 function draw() {
   // game starts on a tutorial screen :)
   // click the start button and start the game
-  
+ background(0)
+
   if (gameRunning === true) {
-  //background(220);
+  ms = millis() - startTime;
   console.log(`${numOfPipes} pipes`);
   
-  // animates water flowing from first pipe vv
-  
-  if(pipeWaterY < 400) {
-    pipeWaterY +=6.7;// not even a timing, just 67
-  }
-  
-  strokeWeight(0);
-  fill('blue');
-  circle(210, pipeWaterY, 16);
-  
-  // FIRST PIPE
+
+  //  water from ALL PIPES 
+  // pipeWaterY is the variable for the first pipe, i gave each pipe an attribute called pipe.dripY so when it's drawing pipe water, i can just steal the pipe.x  
+  drawPipeWater();
     
-  stroke(0);
-  strokeWeight(2);
-  fill('gray');
-  rect(200, -1, 20, 50);
-  strokeWeight(0);
-  
+  // uhhh RANDOM PIPES  
+    
+  drawPipes();//before randomPipe so the logic will work, idk why but it works
+
+  // FIRST PIPE
+firstPipe();
+    
   // WATER
   hotWaterRises();
+    
+  // timer on bottom in seconds
+    if (noTimer === true) {
+      //nothing
+    } else {
+      fill('yellow')
+      text(`${round(ms, 0)}`, 5, 400) // for debug
+      fill('blue')
+      timeSurvived = round(ms / 1000, 2);
+    }
   
   // GAME OVER SCREEN
-  let roomFull = waterHeight > height;
-  gameOver(roomFull);//game over if room is full
+  gameOver();
   
-  // DRAW RANDOM PIPE
-  ms = millis() - startTime;
+  // ADD RANDOM PIPE TO ARRAY
+  // also funny i made infinite loop with numOfPipes 
+  // this adds a new pipe every timeGap (if tG = 1000, then it waits a thousand millisecs) to the array
   
-  pipeX = random(0, width);
- 
-  stroke('blue')
-  fill('white')
+  if (ms - lastPipeTime > timeGap) {
+    randomPipe();
+    lastPipeTime = ms; // Update the timestamp
+    timeGap -= difficulty;// makes pipes appear faster by decrementing the gap between pipe placements in miliseconds!
+  }
   
-  // displays passing ms (remove later)
-  text(`${round(ms, 1)}`, 15, 350, 350)
-  
-  //also funny i made infinite loop with numOfPipes 
-  
-// Create a new pipe every second
-if (ms - lastPipeTime > 1000) {
-  randomPipe();
-  lastPipeTime = ms; // Update the timestamp
-}// end of randomPipe maker
+  if (numOfPipes < 0) {//avoids negatives
+    numOfPipes = 0;
+  }
     
   } else {
     // runs when gameRunning is false
@@ -96,30 +161,93 @@ if (ms - lastPipeTime > 1000) {
 } // end of draw
 
 
-
-function randomPipe() {
-  if (noPipes === true) {
-   //dont do anything 
-  } else {
-  pipeX = random(0, width);
+function mousePressed() {
+  for (let p of pipeList) {
+    if(p.boolIsClicked() === true) {
+      p.disappear();
+      
+      let i = pipeList.indexOf(p);
+      pipeList.splice(i, 1);// remove pipefrom array so it isn't drawn
+      console.log(i)
+      pipesClicked++;
+      numOfPipes -= 1;
+      break;//stop loop after finding the pipe that was clicked
+    }
+  }
   
-  stroke(0);
-  strokeWeight(2);
-  fill('gray');
-  rect(pipeX, -1, 20, 50);
-
-  //when a pipe appears, this grows
-  numOfPipes += 1;
+  // checkes if the first pipe was clicked bc mousePressed = true wont execute more than once a second
+  if (mouseX > 200 && mouseX < 200 + pipeWidth && mouseY > 0 && mouseY < pipeHeight && noPipes !== true) {// there was a bug where you cloud click that spot on the restart screen and get points
+  fill('black');
+  rect(200, 0, pipeWidth, height - waterHeight)
+  firstPipeIsOnScreen = false;
+  pipesClicked++;
+  numOfPipes = numOfPipes - 1;
   }
 }
 
 
+function drawPipeWater() {
+  if(pipeWaterY < height) {
+    // not even a timing, just 67
+    pipeWaterY +=6.7;
+    fill('blue');
+    circle(210, pipeWaterY, 16);
+  }
 
-function gameOver(isRoomFull) {
+  // these are for the other pipes
+  for (let p of pipeList) {
+    if (p.dripY < height) {
+      p.dripY += 16.7; // increments faster bc ill click the pipes before it reaches the water and i figured it would get cluttered with all the random water drops
+      fill('blue');
+      circle(p.x, p.dripY, 16);
+    }
+  }
+}
+
+
+function firstPipe() {
+  if (firstPipeIsOnScreen) {
+  fill('gray');
+  strokeWeight(0);
+  rect(200, -1, pipeWidth, pipeHeight); 
+  }
+   noStroke();
+}
+
+
+function randomPipe() {
+  strokeWeight(0);
+  if (noPipes === true) {
+   //dont do anything 
+  } else {
+  
+  let newestPipe = newPipe();
+  pipeList.push(newestPipe);//adds a new random pipe to the public array, accessible by the next function
+    
+  //when a pipe appears, this grows
+  numOfPipes += 1;
+  }
+  noStroke();
+}
+
+
+function drawPipes() {
+  for (let p of pipeList) {
+      p.draw();
+  }
+}
+
+
+function gameOver() {
+  let isRoomFull = waterHeight > height;
   if (isRoomFull === true) {
     fill('yellow');
     text('Game Over', 0, 300);
+    text(`${timeSurvived} Seconds Survived`, 0, 325);
+    text(`${pipesClicked} Pipes Clicked`, 0, 350);
     noPipes = true;
+    noTimer = true;
+    pipeList = [];
     newGameButton();
   }
 }
@@ -151,21 +279,27 @@ room with water so you don't drown!`,
     gameRunning = true;
     background(0);// clears the screen
     frameCount = 0; // for calcWaterHeight
+
+    timeSurvived = 0;
+    pipesClicked = 0;
     waterHeight = 0;
     pipeWaterY = 0;
     numOfPipes = 1;
+    noTimer = false;
     lastPipeTime = 0;
+    timeGap = gameLengthener;// the initial wait time for the second pipe, also lengthens the game when you mess with the second variable
+    firstPipeIsOnScreen = true;
     noPipes = false;// it was drawing pipes on the start new game screen soooooooo
     previousHeightOfWater = 0;
+    ms = 0;
     startTime = millis();
+    
   }
 }
 
 
-function calculateWaterHeight() {
-  //THE DRAIN, drains water when there are no pipes on screen
-  let drainRate = 1 / 4;
-  
+
+function calculateWaterHeight() {  
   let waterGrowthRate = numOfPipes / 5;
   
   let heightOfWater = waterGrowthRate + previousHeightOfWater - drainRate;
@@ -187,7 +321,6 @@ function hotWaterRises() {
   waterHeight = calculateWaterHeight();
   
   // starts at the bottom of the screen
-  stroke("blue")
   fill("blue")
   rect(0, height - waterHeight, waterWidth, waterHeight);
   }
